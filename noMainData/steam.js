7d41e96a1bbec0a3e455796fbe6a7ca808d2f4d0
pgorokhov <pgorokhov@kpmg.ru>
2018-11-19 02:03:08 +0300

    add post

--- noMainData/steam.js
+++ noMainData/steam.js
@@ -10,3 +10,26 @@ var fs = require('fs');
         
         console.log(chunk);
     });
+
+    // var productRouter = express.Router();
+// productRouter.route('/')
+//     .get(function(request, response){
+//         response.send('list of products');
+//     });
+// productRouter.route("/:id")
+//     .get(function(request, response){
+//         response.send(`Product is ${request.params.id}`);
+//     });
+
+// app.use('/products', productRouter);
+// app.use('/products', function(request, response, next){
+
+//     var nowDt = new Date();
+//     var hour = nowDt.getHours();
+//     var min  = nowDt.getMinutes();
+//     var sec  = nowDt.getSeconds();
+//     var data = `${hour}:${min}:${sec}:${request.method}:${request.url}:${request.get('user-agent')}`;
+//     console.log(data);
+//     fs.appendFile('serverApp/server.log', data+'\n', function(){});
+//     next();
+// });
