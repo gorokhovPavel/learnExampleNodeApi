73f1a9898ed553c4c3bd009f62ea401e7fd36ac1
gorokhovPavel <pgorokhov@yandex.ru>
2019-10-23 05:02:50 +0300

    add new gets

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,10 +1,15 @@
 const Category = require('../models/Category');
+const { formatResponse } = require('../libs/formatResponse');
 
 module.exports.categoryList = async function categoryList(ctx, next) {
-  
   const response = await Category.find();
-  const categoryArr = response.map(x=>{
-    return x;
-  })
-  ctx.body = {categories : categoriesArr};
-};
+  const categoryList = response.map((model) => {
+    model = formatResponse(model._doc);
+    model.subcategories = model.subcategories.map((subcategory) => {
+      return formatResponse(subcategory._doc);
+    });
+    return model;
+  });
+
+  ctx.body = { catrgories : categoryList };
+}
--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,50 @@
+const {formatResponse} = require('./../libs/formatResponse');
+const mongoose = require('mongoose');
 const Product = require('../models/Product');
 
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-    ctx.body = {products: []};
-  };
   
-  module.exports.productList = async function productList(ctx, next) {
-    ctx.body = {products: []};
-  };
+  const {subcategory} = ctx.request.query;
+  //if (!subcategory || subcategory.length === 0) return;
   
-  module.exports.productById = async function productById(ctx, next) {
-    ctx.body = {product: {}};
-  };
+  ctx.productsList = await Product.find({subcategory});
+};
+
+module.exports.productList = async function productList(ctx, next) {
+  const {productsList} = ctx;
+  ctx.body = {products: productsList.map((product) => formatResponse(product._doc))};
+};
+
+module.exports.productById = async function productById(ctx, next) {
   
+  const {id} = ctx.params;
+  try {
+    new mongoose.Types.ObjectId(id);
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = 'Bad id';
+    return;
+  }
+
+  let Product = {};
+  let resp = {};
+
+  try {
+    Product = await require('./../models/Product');
+    resp = await Product.findById(id);
+  } catch (err) {
+    await Product.db.close();
+    throw err;
+  }
+
+  if (!resp) {
+    ctx.status = 404;
+    ctx.body = 'Product not found!';
+
+    return;
+  }
+
+  ctx.body = {product: formatResponse(resp._doc)};
+
+  await next();
+};
