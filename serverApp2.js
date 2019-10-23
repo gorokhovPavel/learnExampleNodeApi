3acc905855b49afe3f557ac3473af4c216d7bc17
gorokhovPavel <pgorokhov@yandex.ru>
2019-10-23 03:08:08 +0300

    add product controller

--- serverApp2.js
+++ serverApp2.js
@@ -1,8 +1,8 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-//const {productsBySubcategory, productList, productById} = require('./controllers/products');
+const {productsBySubcategory, productList, productById} = require('./controllers/products');
 const {categoryList} = require('./controllers/categories');
-
+ 
 const app = new Koa();
 app.use(require('koa-bodyparser')());
 app.use(async (ctx, next) => {
@@ -28,8 +28,8 @@ app.use(async (ctx, next) => {
 const router = new Router({prefix: '/api'});
 
 router.get('/categories', categoryList);
-//router.get('/products', productsBySubcategory, productList);
-//router.get('/products/:id', productById);
+router.get('/products', productsBySubcategory, productList);
+router.get('/products/:id', productById);
 
 app.use(router.routes());
 
