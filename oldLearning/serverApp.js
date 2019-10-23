6b7f4c5510a154cdb92cae317951699b61f4306a
gorokhovPavel <pgorokhov@yandex.ru>
2019-10-23 04:36:46 +0300

    add posts

--- oldLearning/serverApp.js
+++ oldLearning/serverApp.js
@@ -1,30 +1,58 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
+const Koa = require('koa');
+const Router = require('koa-router');
+const {productsBySubcategory, productList, productById} = require('./controllers/products');
+const {categoryList} = require('./controllers/categories');
 
-const server = new http.Server();
+const Category = require('./models/Category');
+const Product = require('./models/Product');
 
-server.on('request', (req, res) => {
-
-    const pathName = url.parse(req.url).pathname.slice(1);
-    const filePath = path.join( __dirname, 'files', pathName );
-    const urlArr = req.url.split('/');
-    
-    if( urlArr.length > 2 ) {
-        res.statusCode = 400;
-        res.end('Unknown request');
-    } 
-    
-    fs.readFile( filePath, (err, data)=> {
-        if(err) {
-            res.statusCode = 404;
-            res.end('file not found!')
-        } else {
-            res.end(data);
-        }
-    }); 
-    
+const app = new Koa();
+app.use(require('koa-bodyparser')());
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch(err) {
+    if (err.status) {
+      ctx.status = err.status;
+      ctx.body = err.message;
+      return;
+    }
+    if (err.name === 'ValidationError') {
+      ctx.status = 400;
+      ctx.body = Object.keys(err.errors).map(key => ({ [key]: err.errors[key].message }));
+    } else {
+      ctx.status = 500;
+      ctx.body = 'Internal server error';
+      console.error(err);
+    }
+  }
 });
 
-module.exports = server;
+const router = new Router({prefix: '/api'});
+
+router.get('/categories', categoryList);
+router.get('/products', productsBySubcategory, productList);
+router.get('/products/:id', productById);
+
+router.post('/categories', async(ctx)=>{
+  const category = await Category.create({
+    title: ctx.request.body.title,
+    subcategories: ctx.request.body.subcategories
+  });
+  ctx.body = category;
+});
+
+router.post('/products', async(ctx)=>{
+  const products = await Product.create({
+    title : ctx.request.body.title,
+    images : ctx.request.body.images,
+	  category : ctx.request.body.category,
+    subcategory : ctx.request.body.subcategory,
+    price : ctx.request.body.price,
+    description : ctx.request.body.description
+  });
+  ctx.body = products;
+});
+
+app.use(router.routes());
+module.exports = app;
--- serverApp2.js
+++ serverApp2.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const {productsBySubcategory, productList, productById} = require('./controllers/products');
 const {categoryList} = require('./controllers/categories');
- 
+
 const app = new Koa();
 app.use(require('koa-bodyparser')());
 app.use(async (ctx, next) => {
@@ -32,5 +32,4 @@ router.get('/products', productsBySubcategory, productList);
 router.get('/products/:id', productById);
 
 app.use(router.routes());
-
 module.exports = app;
