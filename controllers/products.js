131b19066937d3b2f5298cf1d11b49b64f0b6f95
gorokhovPavel <pgorokhov@yandex.ru>
2019-10-23 04:00:33 +0300

    small ref on folders

--- controllers/products.js
+++ controllers/products.js
@@ -1,1 +1,14 @@
 const Product = require('../models/Product');
+
+module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
+    ctx.body = {products: []};
+  };
+  
+  module.exports.productList = async function productList(ctx, next) {
+    ctx.body = {products: []};
+  };
+  
+  module.exports.productById = async function productById(ctx, next) {
+    ctx.body = {product: {}};
+  };
+  
--- models/Product.js
+++ models/Product.js
@@ -1,26 +1,35 @@
 const mongoose = require('mongoose');
-const connection = require('../lib/connection');
+const connection = require('../libs/connection');
 
-const schema = new mongoose.Schema({
-    title : {
-        type : String,
-        required : true
-    },
-    description : {
-        type : String,
-        required : true
-    },
-    price : {
-        type : Number,
-        required : true
-    },
-    subcategory : {
-        type : mongoose.Schema.Types.ObjectId,
-        required : true
-    },
-    images : [{
-        type : String
-    }]
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+  },
+
+  category: {
+    type: mongoose.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+
+  subcategory: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+
+  images: [String],
+
 });
 
-module.exports = connection.model('Product', schema);
+module.exports = connection.model('Product', productSchema);
