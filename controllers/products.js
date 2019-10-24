6d28383fef6dcfcd5b2707ebd548d30fdcafbe25
gorokhovPavel <pgorokhov@yandex.ru>
2019-10-24 18:02:10 +0300

    up products

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,38 @@
 const {formatResponse} = require('./../libs/formatResponse');
 const mongoose = require('mongoose');
 const Product = require('../models/Product');
+const Category = require('../models/Category');
 
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  
-  const {subcategory} = ctx.request.query;
-  //if (!subcategory || subcategory.length === 0) return;
-  
-  ctx.productsList = await Product.find({subcategory});
+  try {
+
+    let category = await Category.create({
+      title: 'Category2',
+      subcategories: [{
+        title: 'Subcategory2',
+      }],
+    });
+
+    let product = await Product.create({
+      title: 'Product2',
+      description: 'Description2',
+      price: 10,
+      category: category.id,
+      subcategory: category.subcategories[0].id,
+      images: ['image1'],
+    });
+
+    //Пытаемся передать данные в следующий стрим
+    const productsArr = await Product.find();
+    ctx.productsList = productsArr;
+    await next(); 
+  } catch(err) {
+    throw err;
+  }
 };
 
 module.exports.productList = async function productList(ctx, next) {
+  //Подтягиваем данные из предыдущего стрима и передаем в следующий
   const {productsList} = ctx;
   ctx.body = {products: productsList.map((product) => formatResponse(product._doc))};
 };
@@ -26,25 +48,16 @@ module.exports.productById = async function productById(ctx, next) {
     return;
   }
 
-  let Product = {};
-  let resp = {};
+  let productElem = await Product.findById(id);
 
-  try {
-    Product = await require('./../models/Product');
-    resp = await Product.findById(id);
-  } catch (err) {
-    await Product.db.close();
-    throw err;
-  }
-
-  if (!resp) {
+  if (!productElem) {
     ctx.status = 404;
     ctx.body = 'Product not found!';
-
     return;
   }
-
-  ctx.body = {product: formatResponse(resp._doc)};
+  
+  productElem = formatResponse(productElem._doc);
+  ctx.body = { product : formatResponse(productElem) };
 
   await next();
 };
