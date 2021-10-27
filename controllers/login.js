27237916f0265107ba61ccbc744debf73ed15336
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2021-10-27 15:13:54 +0300

    Update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -4,7 +4,6 @@ const User = require('../models/User');
 const users = require('../fixtures/users');
 
 module.exports.login = async function login(ctx, next) {
-  
   await passport.authenticate('local', async (err, user, info) => {
     if (err) 
       throw err;
