a07a21f15e34eba2e7c6677be8c29d8280ac5c3a
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2022-12-14 19:00:31 +0300

    Update passport.js

--- libs/passport.js
+++ libs/passport.js
@@ -4,6 +4,7 @@ const passport = new KoaPassport();
 const localStrategy = require('./strategies/local');
 const facebookStrategy = require('./strategies/facebook');
 const vkontakteStrategy = require('./strategies/vk');
+const okStrategy = require('./strategies/ok');
 const githubStrategy = require('./strategies/github');
 
 passport.use(localStrategy);
