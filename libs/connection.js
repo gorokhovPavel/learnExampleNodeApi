c6bd5efe86f77b46e6ec4010512c63be5138f77c
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2023-02-13 12:46:48 +0300

    Update connection.js

--- libs/connection.js
+++ libs/connection.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const beautifyUnique = require('mongoose-beautiful-unique-validation');
 const config = require('../config');
 
+
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
